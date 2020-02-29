9de605168d1c064787136c126575e592074446fb
Vertado <vkzheny@gmail.com>
2020-02-29 19:21:25 +0300

    Lab_2.2_2var complete

---
+++ Lab_2.2_2var/Lab_2.2_2var/Lab_2.2_2var.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream fout("file_1.txt");
+
+    char Buff[255];
+    gets_s(Buff);
+
+    fout << Buff;
+
+    fout.close();
+
+    ifstream fin("file_1.txt");
+
+    
+    char min[30];
+    char Buff_2[30];
+
+    fin >> min;
+    
+    while (!fin.eof()) {
+        fin >> Buff_2;
+        if (strlen(Buff_2) < strlen(min)) {
+            for (int i = 0; i < 30; i++)
+                min[i] = Buff_2[i];
+        }
+        else if (strlen(Buff_2) == strlen(min) && Buff_2 < min)
+                for (int i = 0; i < 30; i++)
+                    min[i] = Buff_2[i];
+        }
+    printf("min: %s", min);
+
+    fin.close();
+}
+
