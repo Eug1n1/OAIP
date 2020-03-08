811cd9969eba4f7d7ede592b7655b1f69fec2b7c
Vertado <vkzheny@gmail.com>
2020-03-09 01:44:45 +0300

    lab_2_2_var fixed

---
+++ lab_4/lab_4/lab_4.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum Speciality {
+    Information_Systems_and_Technology,
+    Information_Technology_Software,
+    Information_Security_Software_for_Mobile_Systems,
+    Design_of_Electronic_and_Web_publications
+};
+
+struct University{
+    string name;
+    string adress;
+    int speciality[5];
+    int points[5];
+};
+
+int main()
+{
+    
+}
+
