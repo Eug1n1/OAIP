d2a486e4ad0e9ce1a45e0b5f8fb723b9731c076e
Vertado <vkzheny@gmail.com>
2020-03-15 17:20:34 +0300

    sogj

---
+++ lab_4.2/lab_4.2/lab_4.2.cpp
@@ -0,0 +1,39 @@
+﻿#include <clocale>
+#include <iostream>
+
+using namespace std;
+
+union Utypes
+{
+    char name[50];
+    char ad[50];
+    char spec[20][10];
+    int ball[10];
+    int sum;
+} info;
+
+int main(void)
+{
+    setlocale(LC_ALL, "ru");
+    int size;
+    cout << "Давайте введем информацию про университеты\n";
+    cout << "Количество университетов: "; cin >> size;
+    for (int i = 0; i < size; i++){
+        int k;
+        cout << "\nНазвание: "; cin >> info.name;
+        cout << "Адрес: "; cin >> info.ad;
+        cout << "Количество специальностей: "; cin >> k;
+        cout << "Введите размер оплаты при договорном обучении: "; cin >> info.sum;
+        cout << "Введите название специальности и конкурс прошлого года по ней\n";
+        for (int j = 0; j < k; j++){
+            cout << (j + 1) << ". "; cin >> info.spec[j]; cin >> info.ball[j];
+        }
+       
+        int min = info.ball[0];
+        for (int j = 0; j < k; j++)
+            if (info.ball[j] < min)
+                min = info.ball[j];
+        cout << "Минимальный балл по университу: " << min;
+        
+    }
+}
