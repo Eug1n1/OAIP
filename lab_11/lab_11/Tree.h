dae85fb15c89e7fc207538140a80856f0f0b05db
Vertado <vkzheny@gmail.com>
2020-05-11 14:14:50 +0300

    d

---
+++ lab_11/lab_11/Tree.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct  Node
+{
+	int key;
+	int count;
+	Node* left;
+	Node* right;
+};
+
+struct Tree
+{
+	int countR = 0;
+	Node* node = nullptr;
+	int counter;
+	void AddNode(int k);
+	void PrintTree(Node* temp, int l);
+	void Delete(int k);
+	bool Balance(Node* temp);
+	void DirectBypass(Node* temp);
+	void ReverseBypass(Node* temp);
+	void countRight(Node* temp);
+};
